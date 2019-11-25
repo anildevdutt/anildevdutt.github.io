cc1937e4498f50be0064a3d91db9c97222ef25e8
Tadepalli <TadepalS@ES.AD.ADP.com>
2019-11-25 20:45:32 +0530

    Updated ID

---
+++ image_dithering/particle.js
@@ -0,0 +1,30 @@
+class Particle {
+    constructor(pos) {
+        this.position = createVector(pos.x, pos.y);
+        this.velocity = createVector(0, 0); 
+        this.target = createVector(pos.x, pos.y);       
+    }
+
+
+    seek() {                
+        let desiredVelocity = p5.Vector.sub(this.target, this.position);
+        let dist = desiredVelocity.mag();
+        let steering = p5.Vector.sub(desiredVelocity, this.velocity);
+        steering.limit(this.maxForce);
+        this.velocity.add(steering);
+        let speed;
+        dist <= this.slowRadious ? speed = map(dist, 0, this.slowRadious, 0, this.maxSpeed) : speed = this.velocity.limit(this.maxSpeed);
+        this.velocity.limit(speed);
+        this.position = this.position.add(this.velocity);        
+    }
+    
+    applyForce(force) {
+        this.velocity.add(force);
+    }
+    
+    show() {        
+        strokeWeight(2);
+        point(this.position.x, this.position.y);
+    }
+}
+
--- image_dithering/sketch.js
+++ image_dithering/sketch.js
@@ -1,4 +1,5 @@
 let img;
+let particles = [];
 
 function preload() {
   img = loadImage("cat.jpg");
@@ -7,29 +8,27 @@ function preload() {
 function setup() {
   createCanvas(1200, 600);
   img.filter(GRAY);
-  image(img, 0, 0, 600, 600);
+  //image(img, 0, 0, 600, 600);
+  img.resize(200, 200);
   img.loadPixels();
   for(let y = 0; y < img.height; y++) {
     for(let x = 0; x < img.width; x++) {      
       let oldR = img.pixels[index(x, y)];
       let oldG = img.pixels[index(x, y) + 1];
       let oldB = img.pixels[index(x, y) + 2]; 
-      if(oldB != oldG || oldG != oldR){
-        console.log("Old error", x, y, index(x, y), img.pixels[index(x, y) - 1], img.pixels[index(x, y)], img.pixels[index(x, y) + 1], img.pixels[index(x, y) + 2], img.pixels[index(x, y) + 3]);
-        break;
-      }
-      
       let factor = 1;
       let newR = round(factor * oldR / 255) * floor(255 / factor);
       let newG = round(factor * oldG / 255) * floor(255 / factor);
       let newB = round(factor * oldB / 255) * floor(255 / factor);
-      if(newB != newG || newG != newR){
-        console.log("New error");
-        break;
-      }
+
       img.pixels[index(x, y)] = newR;
       img.pixels[index(x, y) + 1] = newG;
       img.pixels[index(x, y) + 2] = newB;
+      
+      if(newR == 255){
+        let p = createVector(x * 2, y * 2);
+        particles.push(new Particle(p));
+      }
       
       let errR = oldR - newR;
       let errG = oldG - newG;
@@ -45,7 +44,7 @@ function setup() {
     }
   }
   img.updatePixels();
-  image(img, 600, 0, 600, 600);
+  //image(img, 600, 0, 600, 600);
 }
 
 function addErr(errR, errG, errB, x, y) {  
@@ -58,4 +57,13 @@ function index(x, y)  {
   return (x + y * img.width) * 4;
 }
 
-function draw() {}
+function draw() {
+  background(255);  
+  for(let p of particles) {
+    let force = createVector(accelerationX, accelerationY);
+    p.applyForce(force)      
+    p.seek();
+    p.show();
+  }
+}
+
