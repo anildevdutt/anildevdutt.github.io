ab424fd645ccebcfe42dd6c20134b916aed6267f
Tadepalli <TadepalS@ES.AD.ADP.com>
2019-12-05 20:42:24 +0530

    words_image

---
+++ mosaic/sketch.js
@@ -0,0 +1,15 @@
+let smallImgs = [];
+let bigImg;
+
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+
+function draw() {
+    background(51);
+}
---
+++ words_image/sketch.js
@@ -0,0 +1,105 @@
+let imgMain;
+let thrs = 0.4;
+let scl = 16;
+let wimg;
+let tslider;
+let sslider;
+let button;
+let thrstext;
+let scltext;
+let comments = `Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.Copies a region of pixels from one image to another. If no srcImage is specified this is used as the source. If the source and destination regions aren't the same size, it will automatically resize source pixels to fit the specified target region.`;
+let cmts = [];
+let pos = -1;
+
+function preload() {
+    imgMain = loadImage("image.jpg");
+}
+
+function processesComments() {
+    for(let i = 0; i < comments.length; i++) {
+        if(comments[i].toUpperCase() != comments[i].toLowerCase()) {
+            cmts.push(comments[i]);
+        }
+    }
+}
+
+function nextChar() {
+    pos++;
+    if(pos == cmts.length) {
+        pos = 0;
+    }
+    return cmts[pos];
+}
+
+function setup() {
+    createCanvas(800, 600);
+    processesComments();
+    thrstext = createP("Threshold: 0.4");
+
+    tslider = createSlider(0, 1, 0.4, 0.01);
+    //tslider.position(0.4, 0.4);
+    thrs = tslider.value();
+
+    scltext = createP("Scale: 16");
+    sslider = createSlider(1, 50, 16, 1);
+    //sslider.position(16, 16);
+    scl = sslider.value();
+
+    button = createButton('Update');
+    //button.position(19, 19);
+    button.mousePressed(updateImg);
+
+    button = createButton('Download Image');
+    button.mousePressed(downloadImg);
+
+    imgMain.resize(0, 300);
+
+    w2i();
+}
+
+
+function w2i() {
+    let img = createImage(imgMain.width, imgMain.height);
+    img.copy(imgMain, 0, 0, imgMain.width, imgMain.height, 0, 0, imgMain.width, imgMain.height);
+    img.filter(THRESHOLD, thrs);    
+    wimg = createGraphics(img.width * scl, img.height * scl);
+    wimg.background(255);    
+    wimg.fill(0);
+    wimg.textSize(scl);
+    wimg.textAlign(CENTER, CENTER);
+    img.loadPixels();
+    
+    for(let i = 0; i < img.height; i++) {
+        for(let j = 0; j < img.width; j++) {
+            index = (j + i * img.width) * 4;
+            if(img.pixels[index] == 0) {    
+                
+                wimg.text(nextChar(), j * scl, i * scl);
+            }
+        }
+    }
+
+    img.updatePixels();    
+    resizeCanvas(wimg.width, wimg.height);
+    image(wimg, 0, 0);
+    
+    //saveCanvas(wimg, "wordimg", "jpg");
+}
+
+function updateImg() {
+    thrs = tslider.value();
+    scl = sslider.value();
+    thrstext.elt.textContent = "Threshold: " + thrs;
+    scltext.elt.textContent = "Scale: " + scl;
+    console.log(thrs, scl);
+    w2i();
+
+}
+
+function downloadImg() {
+    saveCanvas(wimg, "wordimg", "jpg");
+}
+
+// function draw() {
+//     //w2i();
+// }   
